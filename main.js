4e9be0f4faaa17a75657b0626d4a4e4f9f06c21c
Juan Antonio Peñalver Rodríguez <juancompositor@gmail.com>
2025-04-23 17:47:30 +0100

    añadido botón hamburguesa al header y cambiado los estilos para adaptarnos a el nuevo botón y el responsive

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 import "./sass/style.scss";
 
+// Efecto sticky con clase 'scrolled'
 window.addEventListener("scroll", () => {
-    const header = document.querySelector("header");
-    if (window.scrollY > 0) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-    }
-  });
+  const header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+// Toggle menú hamburguesa
+const hamburger = document.getElementById("hamburger");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
